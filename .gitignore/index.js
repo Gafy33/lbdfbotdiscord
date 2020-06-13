6cf21e2cb82d360f453062a51454495a9070be0a
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2020-06-14 01:23:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = ("$");
+var prefix = ("lbdf ");
 
 
 //Toutes les actions à faire quand le bot se connecte
 client.on("ready", function () {
-    client.user.setGame("Commande : $help");
+    client.setGame("Commande : $help");
     console.log("Mon BOT est Connecté");
 })
 
@@ -13,11 +13,11 @@ client.login(process.env.TOKEN);
 
 client.on('message', message => {
     if(message.content === prefix + "help"){
-        message.channe("Commandes : \n $dim");
+        message.reply("Commandes : \n lbdf dim");
     }
     
     if(message.content === prefix + "dim"){
-        message.reply("Dimitry est le plus beau mais le plus nul à rocket league");
+        message.channel.sendMessages("Dimitry est le plus beau mais le plus nul à rocket league");
     }
     
 });
