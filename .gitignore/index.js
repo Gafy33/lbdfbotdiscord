94f27ad8576de3d3f6551616f5f875127bdecd8b
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-02-05 22:01:55 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,6 +21,38 @@ client.on('message', message => {
         message.channel.send("tristan c'est le plus beau de tous les gens présent sur cette terre \n ");
     }
     
+    if (String.prototype.startsWith('!abc')) {
+    str.substr(5);
+    message.channel.sendMessage(str);
+    /*var longueur = str.length;
+
+               var i;
+               var poids = 0;
+
+              for(i = 0; i < longueur; i++)
+              {
+                var index = i;
+                var lettre = str.charAt(index);
+
+                if(lettre == "a" ||lettre == "e" || lettre == "i" || lettre == "o" || lettre == "u" || lettre == "y")
+                {
+                    poids = poids + 1;
+                } else if( lettre == "b" ||lettre == "g" || lettre == "l" || lettre == "q" || lettre == "v" ){
+                    poids = poids + 3;
+                } else if( lettre == "f" ||lettre == "k" || lettre == "p" || lettre == "t" || lettre == "z" )
+                {
+                    poids = poids + 2
+                } else if( lettre == "c" ||lettre == "h" || lettre == "m" || lettre == "r" || lettre == "w" )
+                {
+                    poids = poids + ( i - 1 );
+                } else if( lettre == "d" ||lettre == "j" || lettre == "n" || lettre == "s" || lettre == "x" )
+                {
+                    poids = poids -1 ;
+                } 
+
+              }
+
+    message.channel.sendMessage(poids);*/
     
     
     
