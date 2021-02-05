ae021211901825e3774bd0395539668ac1a1d239
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-02-05 22:09:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 var prefix = ("lbdf ");
-
+const uneCommande = '!abc '
 
 //Toutes les actions à faire quand le bot se connecte
 client.on("ready", function () {
@@ -20,39 +20,12 @@ client.on('message', message => {
     if(message.content === prefix + "tristan"){
         message.channel.send("tristan c'est le plus beau de tous les gens présent sur cette terre \n ");
     }
-    
-    if (String.prototype.startsWith('!abc')) {
-    str.substr(5);
-    message.channel.sendMessage(str);
-    /*var longueur = str.length;
 
-               var i;
-               var poids = 0;
-
-              for(i = 0; i < longueur; i++)
-              {
-                var index = i;
-                var lettre = str.charAt(index);
-
-                if(lettre == "a" ||lettre == "e" || lettre == "i" || lettre == "o" || lettre == "u" || lettre == "y")
-                {
-                    poids = poids + 1;
-                } else if( lettre == "b" ||lettre == "g" || lettre == "l" || lettre == "q" || lettre == "v" ){
-                    poids = poids + 3;
-                } else if( lettre == "f" ||lettre == "k" || lettre == "p" || lettre == "t" || lettre == "z" )
-                {
-                    poids = poids + 2
-                } else if( lettre == "c" ||lettre == "h" || lettre == "m" || lettre == "r" || lettre == "w" )
-                {
-                    poids = poids + ( i - 1 );
-                } else if( lettre == "d" ||lettre == "j" || lettre == "n" || lettre == "s" || lettre == "x" )
-                {
-                    poids = poids -1 ;
-                } 
-
-              }
-
-    message.channel.sendMessage(poids);*/
+    if (message.content.startsWith(uneCommande)) {
+    const str = message.content.substring(uneCommande.length)
+    message.channel.sendMessage(str)
+    }
+    });
     
     
     
