8f4a0f02ef6a7a737d4410114306f2b0c99ccad8
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-02-05 22:21:02 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,8 +17,36 @@ client.on('message', message => {
     if (message.content.startsWith(uneCommande)) {
     var str = message.content.substring(uneCommande.length)
     message.channel.send(str)
-    }
+               var longueur = str.length;
+               var i;
+               var poids = 0;
+
+              for(i = 0; i < longueur; i++)
+              {
+                var index = i;
+                var lettre = str.charAt(index);
+
+                if(lettre == "a" ||lettre == "e" || lettre == "i" || lettre == "o" || lettre == "u" || lettre == "y")
+                {
+                    poids = poids + 1;
+                } else if( lettre == "b" ||lettre == "g" || lettre == "l" || lettre == "q" || lettre == "v" )
+                {
+                    poids = poids + 3;
+                } else if( lettre == "f" ||lettre == "k" || lettre == "p" || lettre == "t" || lettre == "z" )
+                {
+                    poids = poids + 2
+                } else if( lettre == "c" ||lettre == "h" || lettre == "m" || lettre == "r" || lettre == "w" )
+                {
+                    poids = poids + ( i - 1 );
+                } else if( lettre == "d" ||lettre == "j" || lettre == "n" || lettre == "s" || lettre == "x" )
+                {
+                    poids = poids -1 ;
+                } 
+
+              }
+        message.channel.send(poids);
     
+    }
     
     
 });
