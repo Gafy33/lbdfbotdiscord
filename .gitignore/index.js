927adae90b5c3f434b83db305da268d3fb5a9304
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-02-06 19:45:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,11 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
-
 const Canvas = require('canvas');
 
-var prefix = ("lbdf ");
-var uneCommande = '!poids ';
-var uneCommande2 = '!ascii ';
+const client = new Discord.Client();
 
-//Toutes les actions à faire quand le bot se connecte
-client.on("ready", function () {
-    client.user.setActivity("Commande : lbdf help");
-    console.log("Mon BOT est Connecté");
-})
-
-client.login(process.env.TOKEN);
-
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
 const applyText = (canvas, text) => {
 	const ctx = canvas.getContext('2d');
@@ -61,66 +52,9 @@ client.on('guildMemberAdd', async member => {
 	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
-
 client.on('message', message => {
-
-    if (message.content.startsWith(uneCommande)) {
-    var str = message.content.substring(uneCommande.length);
-               var longueur = str.length;
-               var i;
-               var poids = 0;
-
-              for(i = 0; i < longueur; i++)
-              {
-                var index = i;
-                var lettre = str.charAt(index);
-
-                if(lettre == "a" ||lettre == "e" || lettre == "i" || lettre == "o" || lettre == "u" || lettre == "y")
-                {
-                    poids = poids + 1;
-                } else if( lettre == "b" ||lettre == "g" || lettre == "l" || lettre == "q" || lettre == "v" )
-                {
-                    poids = poids + 3;
-                } else if( lettre == "f" ||lettre == "k" || lettre == "p" || lettre == "t" || lettre == "z" )
-                {
-                    poids = poids + 2
-                } else if( lettre == "c" ||lettre == "h" || lettre == "m" || lettre == "r" || lettre == "w" )
-                {
-                    poids = poids + ( i - 1 );
-                } else if( lettre == "d" ||lettre == "j" || lettre == "n" || lettre == "s" || lettre == "x" )
-                {
-                    poids = poids -1 ;
-                } 
-
-              }
-        if(poids > 999){
-            message.channel.send("Au putain, Elle est grosse \n celle la !");
-        }
-        message.channel.send("Ta phrase pèse :");
-        message.channel.send(poids + " kg");
-    
-    }
-    
-    if (message.content.startsWith(uneCommande2)) {
-    var str = message.content.substring(uneCommande2.length);
-               var longueur = str.length;
-               var i;
-
-              for(i = 0; i < longueur; i++)
-              {
-                var index = i;
-                var lettre = str.charAt(index);
-
-                if(lettre == "A")
-                {
-                    message.channel.send("```  AAA  \n AA AA \nAA   AA\nAAAAAAA\nAA   AA\n```");
-                }
-
-              }
-    
-    }
-    
-    if (message.content === '!join') {
+	if (message.content === '!join') {
 		client.emit('guildMemberAdd', message.member);
 	}
 });
+
